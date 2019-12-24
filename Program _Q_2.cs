7248e0572572343812f6d3953d2160979200412c
course-tzuri <58888949+course-tzuri@users.noreply.github.com>
2019-12-24 14:11:27 +0200

    Add files via upload

---
+++ Program _Q_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_tzuri_lachmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int secondtotal = 0;
+            int time = 0;
+           
+
+            Console.WriteLine("insert number bigger then 0");
+            secondtotal = Convert.ToInt32(Console.ReadLine());
+            if(secondtotal>=0)
+            {
+                time = secondtotal / 86400;
+                Console.WriteLine("days: {0}", time);
+                time = (secondtotal % 86400)/60/60;
+                Console.WriteLine("hourse: {0}", time);
+                time = (secondtotal % 3600)/60;
+                Console.WriteLine("minute: {0}", time);
+                time = (secondtotal % 60);
+                Console.WriteLine("seconde: {0}", time);
+
+
+
+
+            }
+           else
+                Console.WriteLine("error message: the number should be bigger from 0");
+
+          
+        }
+    }
+}
